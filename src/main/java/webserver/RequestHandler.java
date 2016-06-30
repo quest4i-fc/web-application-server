59f6185cd38e0feb6c7b5f3aef4c9089c0699205
Myeonggu Sa <quest4i@me.com>
2016-06-30 22:16:55 +0900

    [RequestHandler] 1번 과제 완료 /index.html 파일을 읽어서 브라우저로 출력한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,10 +4,16 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.io.File;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -18,13 +24,35 @@ public class RequestHandler extends Thread {
 		this.connection = connectionSocket;
 	}
 
+	
+	@Override
 	public void run() {
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 		
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+		    // GET /index.html HTTP/1.1
+		    
+		    final BufferedReader bfReader = new BufferedReader(new InputStreamReader(in));
+		    String headerLine = null;
+		    String requestURL = null;
+		    while ((headerLine = bfReader.readLine()) != null) {
+		        if (headerLine.contains("GET")) {
+		            requestURL = headerLine.split(" ")[1];
+		        }
+		        if (headerLine.equals("")) {
+		            break;
+		        }
+		        log.debug(headerLine);
+		    }
+		    log.debug("request URL : " + requestURL);
+		    
 			DataOutputStream dos = new DataOutputStream(out);
 			byte[] body = "Hello World".getBytes();
+			if (!(requestURL.equals("/"))) {
+			    body = Files.readAllBytes(new File("./webapp" + requestURL).toPath());
+			}
+
 			response200Header(dos, body.length);
 			responseBody(dos, body);
 		} catch (IOException e) {
