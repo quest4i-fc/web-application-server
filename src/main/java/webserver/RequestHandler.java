b1a6b158a94d841f55531cdc69fb47b9b264ed06
사명구(Sa Myeonggu) <quest4i@me.com>
2016-07-03 21:00:52 +0900

    tab to spaces

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -13,70 +13,66 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.File;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import util.HttpRequestUtils;
 
-
-
 public class RequestHandler extends Thread {
 
-	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-	
-	private Socket connection;
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-	public RequestHandler(Socket connectionSocket) {
-		this.connection = connectionSocket;
-	}
+    private Socket connection;
 
-	
-	@Override
-	public void run() {
-		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
-		
-		try (InputStream in = connection.getInputStream(); 
-		     OutputStream out = connection.getOutputStream()) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-		    // GET /index.html HTTP/1.1
-		    
-		    // 요구사항 0 - HTTP헤더를 출력한다.
-		    final List<String> httpHeader = HttpRequestUtils.getHttpHeaderList(in);
-		    for (final String head : httpHeader) {
-		        log.debug("HTTP Header : {}", head);
-		    }
-		    
-			final DataOutputStream dos = new DataOutputStream(out);
+    public RequestHandler(Socket connectionSocket) {
+        this.connection = connectionSocket;
+    }
 
-		    // 요구사항 1 - index.html 파일을 읽어 클라이언트에 응답한다.
-		    final String path = HttpRequestUtils.getUrl(httpHeader);
-			final byte[] body = HttpRequestUtils.getBody(path);
+    @Override
+    public void run() {
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                connection.getPort());
 
-			response200Header(dos, body.length);
-			responseBody(dos, body);
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	} // end of run()
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // GET /index.html HTTP/1.1
 
-	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-		try {
-			dos.writeBytes("HTTP/1.1 200 OK \r\n");
-			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-			dos.writeBytes("\r\n");
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	}
-	
-	private void responseBody(DataOutputStream dos, byte[] body) {
-		try {
-			dos.write(body, 0, body.length);
-			dos.flush();
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	}
+            // 요구사항 0 - HTTP헤더를 출력한다.
+            final List<String> httpHeader = HttpRequestUtils.getHttpHeaderList(in);
+            for (final String head : httpHeader) {
+                log.debug("HTTP Header : {}", head);
+            }
+
+            final DataOutputStream dos = new DataOutputStream(out);
+
+            // 요구사항 1 - index.html 파일을 읽어 클라이언트에 응답한다.
+            final String path = HttpRequestUtils.getUrl(httpHeader);
+            final byte[] body = HttpRequestUtils.getBody(path);
+
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    } // end of run()
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
 }
